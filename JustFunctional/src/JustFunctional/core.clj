8aea2a89eb1bc0b67dd87cd7c113195ff99f32c6
gavrilovmilos <gavrilovmilos@yahoo.com>
2013-08-24 22:32:46 +0200

    .

--- JustFunctional/src/JustFunctional/core.clj
+++ JustFunctional/src/JustFunctional/core.clj
@@ -1,21 +1,21 @@
 (ns JustFunctional.core
-    (:use ring.adapter.jetty)
-    (:use clojure.string)
     (:use [hiccup.page :only (html5)]
-          [compojure.core :only (defroutes GET)])
-    (:use hiccup.form))
+          [compojure.core :only (defroutes GET)]
+          [hiccup.form]
+          [ring.adapter.jetty]
+          [clojure.string]))
 
 (defn extract-name [uri]
     (replace-first uri "/" ""))
 
 (defn index [par1] (html5
 									    [:head
-									      [:title "Hello World"]]
+									      [:title "Hallo world"]]
 									    [:body
 									      [:div {:id "content"} (str "Index page" par1)]
                [:div {:id "content2"} 
                 [:form {:action "/first" :method "get"}
-	                (label "email" "E-mail: ")
+	                (label "email  2" "E-mail: ")
 	                (text-field "email" "email")
 	                (label "password" "Password: ")
 	                (password-field "password" "password")
@@ -28,30 +28,14 @@
   (GET "/index" [] (index "parametar"))
   (GET "/first" [email pass] (first-page email pass)))
 
-;I used dispetcher function instead of compojure's function defroutes
-;(defn dispetcher [req]
-;    {:status 200
-;     :headers {"Content-Type" "text/html"}
-;     :body    (if (= (:uri req) "/") 
-;                  (index "parametar prvi ")
-;                  (str "Hello, 2222 " (extract-name (:uri req)) "!"))})
+(defn redirect
+	[id]
+		(if-let [url (url-for id)]
+			(ring.util.response/redirect url)
+			{:status 404 :body (str "No such short URL: " id)}))
 
-(defn -main [] (run-jetty #'routes {:port 8080}))
 
+(defn server [] (run-jetty #'routes {:port 8080}))
 
 
 
-;A simple handler to show send some response to the client
-;(defn index 
-;  [req]
-;  (ring.util.response "Welcome page"))
-
-;Routes definition
-;(def routes
-;  (app
-;    [""] index))
-
-;;start function for starting jetty
-;(defn start
-;  ([] (start 8080))
-;  ([port] (run-jetty #'routes {:port port :join? false})))
