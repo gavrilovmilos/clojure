f360a88af29840371d9f38e9d03d22e11331a90a
gavrilovmilos <gavrilovmilos@yahoo.com>
2013-08-25 20:28:26 +0200

    .

--- JustFunctional/src/JustFunctional/core.clj
+++ JustFunctional/src/JustFunctional/core.clj
@@ -1,20 +1,21 @@
 (ns JustFunctional.core
     (:use [hiccup.page :only (html5)]
-          [compojure.core :only (defroutes GET)]
+          [compojure.core :only (defroutes GET POST)]
           [hiccup.form]
           [ring.adapter.jetty]
-          [clojure.string]))
+          [clojure.string]
+          [hiccup.page :only [include-css]]))
 
-(defn extract-name [uri]
-    (replace-first uri "/" ""))
+;(defn page-def-head [title] ([:head
+;									      [:title title] (include-css "/css/layout.css")]))
 
 (defn index [par1] (html5
 									    [:head
-									      [:title "Hallo world"]]
+									      [:title "JustFunctional"] (include-css "/css/layout.css")]
 									    [:body
 									      [:div {:id "content"} (str "Index page" par1)]
                [:div {:id "content2"} 
-                [:form {:action "/first" :method "get"}
+                [:form {:action "/home" :method "post"}
 	                (label "email  2" "E-mail: ")
 	                (text-field "email" "email")
 	                (label "password" "Password: ")
@@ -22,20 +23,13 @@
 	                (submit-button {:id "loginButton"} "Log in")]]]))
 
 (defn first-page [email pass] (str "Welcome: " email pass))
+(defn log-in [params] (str "Home page" params))
 
 (defroutes routes
   (GET "/" [] "<h2>Hello World</h2>")
   (GET "/index" [] (index "parametar"))
-  (GET "/first" [email pass] (first-page email pass)))
-
-(defn redirect
-	[id]
-		(if-let [url (url-for id)]
-			(ring.util.response/redirect url)
-			{:status 404 :body (str "No such short URL: " id)}))
+  (GET "/first" [email pass] (first-page email pass))
+  (POST "/home" params (log-in params)))
 
 
 (defn server [] (run-jetty #'routes {:port 8080}))
-
-
-
