c8b9b8a9805a56e98d214d27a310b28b074f9f85
gavrilovmilos <gavrilovmilos@yahoo.com>
2013-08-31 20:28:06 +0200

    2

--- JustFunctional/src/JustFunctional/core.clj
+++ JustFunctional/src/JustFunctional/core.clj
@@ -1,19 +1,34 @@
 (ns JustFunctional.core
     (:use [hiccup.page :only (html5)]
-          [compojure.core :only (defroutes GET POST)]
           [hiccup.form]
+          [hiccup.page :only [include-css]]
+          [compojure.core :only (defroutes GET POST)]
+          
           [ring.adapter.jetty]
           [clojure.string]
-          [hiccup.page :only [include-css]]))
+          )
+    (:require [compojure.route :as route]
+              [clostache.parser :as clostache]
+              [clostache.route :as route]))
 
 ;(defn page-def-head [title] ([:head
 ;									      [:title title] (include-css "/css/layout.css")]))
+
+
+(defn read-template [template-name] 
+  (slurp (clojure.java.io/resource 
+           (str "templates/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+  (clostache/render (read-template template-file) params))
+
+(defn clos-page [] (render-template "clos" {:greeting "Ciao djaci"}))
 
 (defn index [par1] (html5
 									    [:head
 									      [:title "JustFunctional"] (include-css "/css/layout.css")]
 									    [:body
-									      [:div {:id "content"} (str "Index page" par1)]
+									      [:div {:id "content"} (str "Index page > " par1)]
                [:div {:id "content2"} 
                 [:form {:action "/home" :method "post"}
 	                (label "email  2" "E-mail: ")
@@ -23,13 +38,16 @@
 	                (submit-button {:id "loginButton"} "Log in")]]]))
 
 (defn first-page [email pass] (str "Welcome: " email pass))
-(defn log-in [params] (str "Home page" params))
+(defn log-in [params] (str "Home page " params))
 
 (defroutes routes
   (GET "/" [] "<h2>Hello World</h2>")
   (GET "/index" [] (index "parametar"))
   (GET "/first" [email pass] (first-page email pass))
-  (POST "/home" params (log-in params)))
+  (GET "/clos" [] (clos-page))
+  (POST "/home" params (log-in params))
+  (route/resources "/")
+  (route/not-found "404 Page Not Found"))
 
 
 (defn server [] (run-jetty #'routes {:port 8080}))
