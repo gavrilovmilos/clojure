927a33218c2f9433ca377bbda4e40206b9813924
gavrilovmilos <gavrilovmilos@yahoo.com>
2013-10-09 00:43:13 +0200

    Indexing algorithm implemented and works fine!

--- JustFunctional/src/search/core.clj
+++ JustFunctional/src/search/core.clj
@@ -8,25 +8,39 @@
 
 (defn get-words [line] (split line #"\s+"))
 
-;(def ^:dynamic row 0)
+(defn get-index-list [map1 map2]
+  (for [x map1
+        y map2]
+           (if (= (get x 0) (get y 0))
+               (hash-map (get x 0) (vector (get x 1) (get y 1)))
+               {:d3ef 9}
+             )))
 
-;isparsiraj tekst na linije teksta
-;prolazis kroz svaku liniju uzimas rec i upisujes je u hash mapu
-;npr {:street [1 5 6 2]} gde vektor predstavlja linije teksta na kojima se rec nalazi
+(defn convert-seq-to-map [seqv]
+  (loop [i 0 map {}]
+    (if (< i (count seqv))
+      (recur (inc i) 
+             (merge map (nth seqv i)))
+      map)))
+
+(defn get-same-keys-map [map1 map2] (dissoc (convert-seq-to-map (get-index-list map1 map2)) :d3ef))
+
+(defn just-merge [map1 map2] 
+  (merge map1 map2
+           (get-same-keys-map map1 map2)))
+
+
 (defn index [file-name] 
   (binding [lines (split-lines (slurp file-name))]
-    (loop [i 0 index-map []] 
+    (loop [i 0 index-map {}] 
       (if (< i (count lines))
         (recur (inc i) 
-               (merge index-map 
-                      (loop [j 0 inner-index-map []] 
-                 (if (< j (count (get-words (nth lines i))))
-                   (recur (inc j) 
-                          (do (println (str index-map "red: " i " -- rec : " j " " (nth (get-words (nth lines i)) j) " count: " (count (get-words (nth lines i))) "\n")) 
-                            (merge inner-index-map {(nth (get-words (nth lines i)) j) i})))
-                   inner-index-map))))        
+               (just-merge index-map 
+                      (loop [j 0 inner-index-map {}] 
+                        (if (< j (count (get-words (nth lines i))))
+                          (recur (inc j) 
+;                                 (do (println (str index-map "red: " i " -- rec : " j " " (nth (get-words (nth lines i)) j) " count: " (count (get-words (nth lines i))) "\n")) 
+                                   (just-merge inner-index-map (hash-map (nth (get-words (nth lines i)) j) i)))
+;                          )
+                          inner-index-map))))        
         index-map))))
-
-;(index "resources/content/text.txt")
-;definisi metodu za merge-ovanje index mapa, koja ce prvo proveravati ima li indeksa i ukoliko ima vrsiti update a ukoliko nema
-;vrsiti merge
