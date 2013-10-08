f55c55a411c25b2d281738fe987cf0605e58e4d3
gavrilovmilos <gavrilovmilos@yahoo.com>
2013-10-09 00:49:30 +0200

    Indexing algorithm implemented and works fine!

--- JustFunctional/src/search/core.clj
+++ JustFunctional/src/search/core.clj
@@ -25,11 +25,15 @@
 
 (defn get-same-keys-map [map1 map2] (dissoc (convert-seq-to-map (get-index-list map1 map2)) :d3ef))
 
+;this function merges two hashmaps
+;clojure/core merge function overrides properties with the same ID and the value from the last map is the final one.
+;just-merge function creates a vector for map's properties with the same key. This map has values from both maps.
 (defn just-merge [map1 map2] 
   (merge map1 map2
            (get-same-keys-map map1 map2)))
 
-
+;this function is indexing given text file
+;it uses just-merge function for merging hashmaps
 (defn index [file-name] 
   (binding [lines (split-lines (slurp file-name))]
     (loop [i 0 index-map {}] 
